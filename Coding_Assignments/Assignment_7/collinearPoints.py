29b86889f3e57ff6821ba99de6c09bd74cba5468
Rahul Gandhi <gandhirahul274@gmail.com>
2022-10-23 18:40:17 +0530

    collinear points approach 2

--- Coding_Assignments/Assignment_7/collinearPoints.py
+++ Coding_Assignments/Assignment_7/collinearPoints.py
@@ -11,5 +11,21 @@ def collinearPoints(points):
         return "No"
 
 
-print(collinearPoints([(1, 1), (1, 4), (1, 5)]))
-print(collinearPoints([(1, 1), (1, 6), (0, 9)]))
+def usingAreaOfTriangle(points):
+    p1, p2, p3 = points
+
+    x1, y1 = p1
+    x2, y2 = p2
+    x3, y3 = p3
+
+    area = 0.5 * (x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2))
+    if area == 0:
+        return "Yes"
+    else:
+        return "No"
+
+
+print("Approach 1", collinearPoints([(1, 1), (1, 4), (1, 5)]))
+print("Approach 2", usingAreaOfTriangle([(1, 1), (1, 4), (1, 5)]))
+print("Approach 1", collinearPoints([(1, 1), (1, 6), (0, 9)]))
+print("Approach 2", usingAreaOfTriangle([(1, 1), (1, 6), (0, 9)]))
