1f70b1071c33f76a3298fec01806e6fa16d7ee61
Rahul Gandhi <gandhirahul274@gmail.com>
2022-11-05 12:38:25 +0530

    kth smallest/largest using quick sort

--- Coding_Assignments/Assignment_8/kThLarget.py
+++ Coding_Assignments/Assignment_8/kThLarget.py
@@ -1,9 +1,41 @@
 import heapq
 
 
-def findKthLargest(nums, k):
-    return heapq.nlargest(k, nums)
+## Approach 1
+# def findKthLargest(nums, k):
+#     return heapq.nlargest(k, nums)
 
 
-nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
-print(findKthLargest(nums, 4)[-1])
+# nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+# print("Approach 1:", findKthLargest(nums, 4)[-1])
+
+
+def partition(arr, p, q):
+    pivot = arr[p]
+    i = p
+    for j in range(p + 1, q + 1):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i], arr[p] = arr[p], arr[i]
+    return i
+
+
+def selectionProcedure(arr, p, q, k):
+    idx = partition(arr, p, q)
+    if idx == k:
+        return arr[idx]
+    elif k < idx:
+        return selectionProcedure(arr, p, idx - 1, k)
+    else:
+        return selectionProcedure(arr, idx + 1, q, k)
+
+
+## Approach 2
+def findKthLargestElement(nums, k):
+    return selectionProcedure(nums, 0, len(nums) - 1, len(nums) - k)
+
+
+arr = [40, 25, 68, 79, 52, 66, 89, 97]
+k = 2
+print("Approach 2:", findKthLargestElement(arr, k))
--- Coding_Assignments/Assignment_8/kThSmallest.py
+++ Coding_Assignments/Assignment_8/kThSmallest.py
@@ -1,9 +1,47 @@
 import heapq
 
-
+## Approach 1: Using Heap
 def findKthSmallest(nums, k):
     return heapq.nsmallest(k, nums)
 
 
 nums = [3, 2, 3, 1, 2, 4, 5, 5, 6]
-print(findKthSmallest(nums, 4)[-1])
+print("Approach 1:", findKthSmallest(nums, 4)[-1])
+arr = [40, 25, 68, 79, 52, 66, 89, 97, 39]
+print("Approach 1:", findKthSmallest(arr, 3)[-1])
+
+
+## Approach 2: Using Quick Sort Partition Method
+
+
+def partition(arr, p, q):
+    pivot = arr[p]
+    i = p
+    for j in range(p + 1, q + 1):
+        if arr[j] <= pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+    arr[i], arr[p] = arr[p], arr[i]
+    return i
+
+
+def selectionProcedure(arr, p, q, k):
+    idx = partition(arr, p, q)
+    if idx == k:
+        return arr[idx]
+    elif k < idx:
+        return selectionProcedure(arr, p, idx - 1, k)
+    else:
+        return selectionProcedure(arr, idx + 1, q, k)
+
+
+def findKthMinElement(arr, k):
+    return selectionProcedure(arr, 0, len(arr) - 1, k)
+
+
+arr = [3, 2, 3, 1, 2, 4, 5, 5, 6]
+k = 4
+print("Approach 2:", findKthMinElement(arr, k - 1))
+arr = [40, 25, 68, 79, 52, 66, 89, 97, 39]
+k = 3
+print("Approach 2(Arr 2):", findKthMinElement(arr, k - 1))
